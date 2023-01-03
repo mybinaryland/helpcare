99882ded09e731e4ea3a1f71861cc5666edb3cbe
mybinaryland <webinfo777@gmail.com>
2023-01-03 14:36:46 +0800

    Add files via upload

---
+++ adminindex.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 9px sans-serif; text-align: center; }
+		
+    </style>
+	
+	</head>
+
+<body background="caregiver.jpeg">
+<center>
+ <h1>Welcome</h1>
+</center>
+<p align="center">
+<a href="../admin/adminwelcome.php" class="btn btn-warning"> Services offered </font></a>  
+</p>
+
+ </body>
+ </html>
+ 
+ 
+ 
---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<title><head>Home</head></title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+	
+<body background= "caregiver2.jpg">
+
+
+
+<h1><h1 font size= 22 align="center">Welcome to our Website</h1>
+<p>
+<a href="../client/clientlogin.php" class="btn btn-warning">Client Log in</a>
+<a href="../client/clientregister.php" class="btn btn-danger ml-3">Client Register</a>
+<a href="providerlogin.php" class="btn btn-warning">Provider Log in</a>
+        <a href="providerregister.php" class="btn btn-danger ml-3">Provider Register</a>
+</p>
+</body>
+</html>
